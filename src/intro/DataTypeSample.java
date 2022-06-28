7e00eeb05973c9cba5295928414ad1fe1ab939f7
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-06-28 21:12:32 +0530

    basic

---
+++ src/intro/DataTypeSample.java
@@ -0,0 +1,30 @@
+package intro;
+
+public class DataTypeSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+//		biscuit cream = 10; 
+	    int cost = 10;
+//	    int ->datatype
+//	    cost -> variable name
+//	    = -> assignment operator
+//	    10 -> value 
+	    
+	    int carrot = 20;
+	    byte strawberry = 30;
+	    short age = 20;
+	    long marks = 20L;
+	    float subj = 20.8798f;
+	    double  eng = 23.898897d;
+	    boolean isCheck = true;
+	    boolean  isHealth = false;
+	    char alpha = 'z'; //single quotes 
+	    
+	    
+	 
+	}
+
+}
---
+++ src/intro/Sample.java
@@ -0,0 +1,10 @@
+package intro;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.print("hello buddy");
+		System.out.print("I am ");
+	}
+
+}
